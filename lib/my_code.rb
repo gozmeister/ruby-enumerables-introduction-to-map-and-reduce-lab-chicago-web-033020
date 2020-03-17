1971a143d978867dc3a4d50934f475145dea088c
Sam Gorick <samgorick.sg@gmail.com>
2020-03-17 17:01:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,9 @@
-# My Code here....
+def map_to_negativize(source_array)
+  i = 0 
+  new_array = []
+  while i < source_array.length do
+    new _value = source_array[i] * -1
+    new_value << new_array
+  end 
+  new_array
+end
