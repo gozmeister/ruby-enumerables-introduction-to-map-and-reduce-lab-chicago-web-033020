6287bbdf404318641e02a7c59c50956ceaf1eee9
Sam Gorick <samgorick.sg@gmail.com>
2020-03-17 17:24:43 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -62,3 +62,15 @@ def reduce_to_all_true(source_array)
   end
   true
 end
+
+def reduce_to_any_true(source_array)
+  i = 0 
+  while i < source_array.length do 
+    if source_array[i] == true 
+      return true
+    else 
+      i += 1 
+    end
+  end 
+  false 
+end
